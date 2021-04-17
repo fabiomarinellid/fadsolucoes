ed92efcc4e7963e3302e4905cb64083842ffe3f6
fabio Marinelli <fabio.marinellidionizio>
2021-04-17 11:31:35 -0300

    layout heroku atualizado

---
+++ index.php
@@ -0,0 +1,8 @@
+<?php header( 'Location: /index.html' ) ;  ?>
+
+<meta property="og:url" content="https://fadsolucoes.com/index.html" />
+<meta property="og:type" content="website" />
+<meta property="og:title" content="FAD Soluções - Soluções em Comunicação Visual" />
+<meta property="og:description" content="Soluções em Comunicação Visual para Jaú e região" />
+<meta property="og:image" content="assets/images/social-image.png" />
+<meta property="fb:app_id" content="1118503441868272" />
